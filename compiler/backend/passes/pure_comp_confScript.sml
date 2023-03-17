CakeML/pure

commit 391fc8e7864ddaa440c634f1c2198b05c35e434b

Add compiler flag for `mk_delay` optimisation

compiler/backend/passes/pure_comp_confScript.sml
@@ -11,7 +11,8 @@ Datatype:
       do_pure_sort  : bool ;  (* pure-to-pure binding group analysis *)
       do_pure_clean : bool ;  (* pure-to-pure cleaning / deadcode elimination *)
       do_demands    : bool ;  (* demand analysis *)
-      do_split_dlam : bool ;  (* thunk-to-thunk *)
+      do_mk_delay   : bool ;  (* thunk-to-thunk smart mk_delay constructor *)
+      do_split_dlam : bool ;  (* thunk-to-thunk split delayed lambdas *)
       do_app_unit   : bool    (* state-to-state *)
     |>
 End
@@ -19,6 +20,7 @@ End
 Overload pure_sort_flag[local]  = “strlit "-sort"”
 Overload pure_clean_flag[local] = “strlit "-clean"”
 Overload demands_flag[local]    = “strlit "-demands"”
+Overload mk_delay_flag[local]   = “strlit "-mk_delay"”
 Overload dlam_flag[local]       = “strlit "-dlam"”
 Overload unit_flag[local]       = “strlit "-unit"”
 
@@ -26,6 +28,7 @@ Definition all_flags_def:
   all_flags = [pure_sort_flag;
                pure_clean_flag;
                demands_flag;
+               mk_delay_flag;
                dlam_flag;
                unit_flag]
 End
@@ -37,7 +40,8 @@ Definition read_cline_args_def:
     | [] => INL <| do_pure_sort  := ¬ MEM pure_sort_flag cl  ;
                    do_pure_clean := ¬ MEM pure_clean_flag cl ;
                    do_demands    := ¬ MEM demands_flag cl    ;
-                   do_split_dlam := ¬ MEM dlam_flag cl       ;
+                   do_mk_delay   := ¬ MEM dlam_flag cl       ;
+                   do_split_dlam := ¬ MEM mk_delay_flag cl   ;
                    do_app_unit   := ¬ MEM unit_flag cl       |>
 End
 

compiler/backend/passes/proofs/pure_to_thunkProofScript.sml
@@ -94,24 +94,25 @@ Proof
 QED
 
 Theorem IMP_mk_delay_eq_Delay:
-  exp_rel x1 y1 ∧ (∀c v. x1 = Var c v ⇒ mk_delay y1 ≠ Var v) ⇒
-  ∃x2. exp_rel x1 x2 ∧ mk_delay y1 = Delay x2
+  exp_rel x1 y1 ∧ (∀c v. x1 = Var c v ⇒ mk_delay flag y1 ≠ Var v) ⇒
+  ∃x2. exp_rel x1 x2 ∧ mk_delay flag y1 = Delay x2
 Proof
   Cases_on ‘∃c m. x1 = Var c m’ \\ gvs []
-  >- (simp [Once exp_rel_cases] \\ rw [] \\ fs [mk_delay_def])
-  \\ strip_tac
-  \\ first_assum $ irule_at $ Pos hd
-  \\ pop_assum mp_tac
-  \\ Cases_on ‘x1’ \\ fs []
-  \\ simp [Once exp_rel_cases] \\ rw []
-  \\ fs [mk_delay_def]
+  >- (
+    simp [Once exp_rel_cases] \\ rw [] \\ fs [mk_delay_def, AllCaseEqs()] >>
+    simp[Once exp_rel_cases]
+    ) >>
+  rw[Once exp_rel_cases] >>
+  simp[Once exp_rel_cases] >> gvs[PULL_EXISTS] >>
+  goal_assum $ drule_at Any >> simp[mk_delay_def]
 QED
 
 Theorem cns_arities_mk_delay:
-  ∀e. cns_arities (mk_delay e) = cns_arities e
+  ∀e flag. cns_arities (mk_delay flag e) = cns_arities e
 Proof
   Cases \\ simp [mk_delay_def, thunk_cexpTheory.cns_arities_def]
   \\ CASE_TAC \\ simp [thunk_cexpTheory.cns_arities_def]
+  \\ Cases \\ simp[thunk_cexpTheory.cns_arities_def]
 QED
 
 Theorem must_delay_in_monad_cns:
@@ -122,28 +123,30 @@ Proof
 QED
 
 Theorem boundvars_exp_of_mk_delay:
-  ∀e. boundvars (exp_of (mk_delay e)) = boundvars (exp_of e)
+  ∀e flag. boundvars (exp_of (mk_delay flag e)) = boundvars (exp_of e)
 Proof
   Cases \\ simp [mk_delay_def, Once exp_of_def, boundvars_def]
   \\ CASE_TAC \\ simp [exp_of_def, boundvars_def]
+  \\ Cases \\ simp[exp_of_def, boundvars_def]
 QED
 
 Theorem cexp_wf_mk_delay:
-  ∀e. cexp_wf (mk_delay e) = cexp_wf e
+  ∀e flag. cexp_wf (mk_delay flag e) = cexp_wf e
 Proof
   Cases \\ simp [mk_delay_def, thunk_exp_ofTheory.cexp_wf_def]
   \\ CASE_TAC \\ simp [thunk_exp_ofTheory.cexp_wf_def]
+  \\ Cases \\ simp [thunk_exp_ofTheory.cexp_wf_def]
 QED
 
 Theorem exp_rel_to_thunk:
-  (∀s (x:'a pure_cexp$cexp) x1 s1.
-    to_thunk s x = (x1,s1) ∧ NestedCase_free x ∧ vars_ok s ∧
+  (∀flag s (x:'a pure_cexp$cexp) x1 s1.
+    to_thunk flag s x = (x1,s1) ∧ NestedCase_free x ∧ vars_ok s ∧
     allvars_of x ⊆ set_of s ∧ cexp_wf x ∧ letrecs_distinct (exp_of x) ⇒
     exp_rel x x1 ∧ set_of s ⊆ set_of s1 ∧ vars_ok s1 ∧ boundvars (exp_of x1) ⊆ set_of s1 ∧
     cexp_wf x1 ∧
     cns_arities x1 ⊆ IMAGE (IMAGE (explode ## I)) (cns_arities x)) ∧
-  (∀s (xs:('a pure_cexp$cexp) list) xs1 s1.
-    to_thunk_list s xs = (xs1,s1) ∧ EVERY NestedCase_free xs ∧ vars_ok s ∧
+  (∀flag s (xs:('a pure_cexp$cexp) list) xs1 s1.
+    to_thunk_list flag s xs = (xs1,s1) ∧ EVERY NestedCase_free xs ∧ vars_ok s ∧
     EVERY (λx.  allvars_of x ⊆ set_of s) xs ∧ EVERY cexp_wf xs ∧
     EVERY (λe. letrecs_distinct (exp_of e)) xs ⇒
     LIST_REL exp_rel xs xs1 ∧ set_of s ⊆ set_of s1 ∧ vars_ok s1 ∧
@@ -617,9 +620,9 @@ Theorem to_thunk_itree_of:
   safe_itree (pure_semantics$itree_of (exp_of x)) ∧
   letrecs_distinct (exp_of x) ⇒
   pure_semantics$itree_of (exp_of x) =
-  thunk_semantics$itree_of (exp_of (FST (to_thunk (pure_names x) x)))
+  thunk_semantics$itree_of (exp_of (FST (to_thunk flag (pure_names x) x)))
 Proof
-  Cases_on ‘to_thunk (pure_names x) x’ \\ fs []
+  Cases_on ‘to_thunk flag (pure_names x) x’ \\ fs []
   \\ drule (exp_rel_to_thunk |> CONJUNCT1) \\ fs []
   \\ fs [pure_names_ok,pure_names_eq_allvars]
   \\ fs [pure_semanticsTheory.itree_of_def]
@@ -633,10 +636,10 @@ Theorem IMP_to_thunk_cexp_wf:
   closed (exp_of x) ∧
   letrecs_distinct (exp_of x) ∧
   NestedCase_free x ⇒
-  thunkLang$closed (thunk_exp_of$exp_of (FST (to_thunk (pure_names x) x)))
+  thunkLang$closed (thunk_exp_of$exp_of (FST (to_thunk flag (pure_names x) x)))
 Proof
   strip_tac
-  \\ Cases_on ‘to_thunk (pure_names x) x’
+  \\ Cases_on ‘to_thunk flag (pure_names x) x’
   \\ drule_then assume_tac (exp_rel_to_thunk |> CONJUNCT1)
   \\ gs [pure_names_ok,pure_names_eq_allvars]
   \\ dxrule_then assume_tac exp_rel_imp_combined
@@ -658,16 +661,17 @@ Theorem compile_to_thunk_itree_of:
   thunk_semantics$itree_of (exp_of (compile_to_thunk c x))
 Proof
   rw [compile_to_thunk_def]
-  \\ Cases_on ‘to_thunk (pure_names x) x’ \\ fs []
+  \\ Cases_on ‘to_thunk c.do_mk_delay (pure_names x) x’ \\ fs []
   \\ irule_at Any EQ_TRANS
-  \\ irule_at Any to_thunk_itree_of
+  \\ irule_at Any to_thunk_itree_of \\ qexists `c.do_mk_delay`
   \\ drule (exp_rel_to_thunk |> CONJUNCT1) \\ fs []
   \\ fs [pure_names_ok,pure_names_eq_allvars]
   \\ rw [thunk_semanticsTheory.itree_of_def]
   \\ pairarg_tac \\ fs []
   \\ drule_then mp_tac split_delated_lam_soundness
   \\ impl_tac \\ simp []
-  \\ drule_all_then assume_tac IMP_to_thunk_cexp_wf
+  \\ drule_all IMP_to_thunk_cexp_wf
+  \\ disch_then $ qspec_then `c.do_mk_delay` assume_tac
   \\ gs []
 QED
 
@@ -682,14 +686,16 @@ Theorem IMP_thunk_cexp_wf:
               IMAGE (IMAGE (explode ## I)) (cns_arities x)
 Proof
   fs [compile_to_thunk_def] \\ strip_tac
-  \\ Cases_on ‘to_thunk (pure_names x) x’ \\ fs []
+  \\ Cases_on ‘to_thunk c.do_mk_delay (pure_names x) x’ \\ fs []
   \\ drule_then assume_tac (exp_rel_to_thunk |> CONJUNCT1)
   \\ gs [pure_names_ok,pure_names_eq_allvars]
-  \\ drule_all_then assume_tac IMP_to_thunk_cexp_wf
+  \\ drule_all IMP_to_thunk_cexp_wf
+  \\ disch_then $ qspec_then `c.do_mk_delay` assume_tac
   \\ pairarg_tac \\ fs []
   \\ drule_then mp_tac split_delated_lam_soundness
-  \\ impl_tac \\ simp []
-  \\ drule_all_then assume_tac IMP_to_thunk_cexp_wf
+  \\ impl_tac \\ gvs []
+  \\ drule_all IMP_to_thunk_cexp_wf
+  \\ disch_then $ qspec_then `c.do_mk_delay` assume_tac
   \\ gs [] \\ rw [] \\ fs []
 QED
 

compiler/backend/passes/pure_to_thunkScript.sml
@@ -24,10 +24,12 @@ Definition get_var_name_def:
 End
 
 Definition mk_delay_def:
-  mk_delay (x:thunk_cexp$cexp) =
-    case x of
-    | Force (Var n) => Var n
-    | _             => Delay x
+  mk_delay flag (x:thunk_cexp$cexp) =
+    if flag then
+      case x of
+      | Force (Var n) => Var n
+      | _             => Delay x
+    else Delay x
 End
 
 Definition must_delay_def:
@@ -42,58 +44,58 @@ Definition must_delay_def:
 End
 
 Definition to_thunk_def:
-  to_thunk (s:vars) (pure_cexp$Var c v) =
+  to_thunk flag (s:vars) (pure_cexp$Var c v) =
     (thunk_cexp$Force (Var v),s) ∧
-  to_thunk s (Lam c ns x) =
-    (let (x,s) = to_thunk s x in (Lam ns x,s)) ∧
-  to_thunk s (App c x ys) =
-    (let (x,s) = to_thunk s x in
-     let (ys,s) = to_thunk_list s ys in
-       (App x (MAP mk_delay ys), s)) ∧
-  to_thunk s (Letrec c xs y) =
-    (let (y,s) = to_thunk s y in
-     let (ys,s) = to_thunk_list s (MAP SND xs) in
+  to_thunk flag s (Lam c ns x) =
+    (let (x,s) = to_thunk flag s x in (Lam ns x,s)) ∧
+  to_thunk flag s (App c x ys) =
+    (let (x,s) = to_thunk flag s x in
+     let (ys,s) = to_thunk_list flag s ys in
+       (App x (MAP (mk_delay flag) ys), s)) ∧
+  to_thunk flag s (Letrec c xs y) =
+    (let (y,s) = to_thunk flag s y in
+     let (ys,s) = to_thunk_list flag s (MAP SND xs) in
        (Letrec (MAP2 (λ(n,_) y. (n, Delay y)) xs ys) y,s)) ∧
-  to_thunk s (Let c v x y) =
-    (let (x,s) = to_thunk s x in
-     let (y,s) = to_thunk s y in
-       (Let (SOME v) (mk_delay x) y,s)) ∧
-  to_thunk s (Prim c p ys) =
-    (let (xs,s) = to_thunk_list s ys in
+  to_thunk flag s (Let c v x y) =
+    (let (x,s) = to_thunk flag s x in
+     let (y,s) = to_thunk flag s y in
+       (Let (SOME v) (mk_delay flag x) y,s)) ∧
+  to_thunk flag s (Prim c p ys) =
+    (let (xs,s) = to_thunk_list flag s ys in
        case p of
        | Cons t => (Prim (Cons t) (if must_delay t
                                    then MAP Delay xs
-                                   else MAP mk_delay xs),s)
+                                   else MAP (mk_delay flag) xs),s)
        | AtomOp a => (Prim (AtomOp a) xs,s)
        | Seq =>
            let x = any_el 0 xs in
            let y = any_el 1 xs in
            let (fresh,s) = invent_var (get_var_name ys) s in
              (Let (SOME fresh) x y,s)) ∧
-  to_thunk s (Case c x v ys opt) =
-    (let (x,s) = to_thunk s x in
-     let (rs,s) = to_thunk_list s (MAP (SND o SND) ys) in
+  to_thunk flag s (Case c x v ys opt) =
+    (let (x,s) = to_thunk flag s x in
+     let (rs,s) = to_thunk_list flag s (MAP (SND o SND) ys) in
      let (w,s) = invent_var (v ^ strlit "_forced") s in
        case opt of
        | NONE =>
-           ((Let (SOME v) (mk_delay x) $
+           ((Let (SOME v) (mk_delay flag x) $
              Let (SOME w) (Force (Var v)) $
               Case w (MAP2 (λ(c,n,_) y. (c,n,y)) ys rs) NONE,s))
        | SOME (a,y) =>
-          let (y,s) = to_thunk s y in
-            ((Let (SOME v) (mk_delay x) $
+          let (y,s) = to_thunk flag s y in
+            ((Let (SOME v) (mk_delay flag x) $
               Let (SOME w) (Force (Var v)) $
                Case w (MAP2 (λ(c,n,_) y. (c,n,y)) ys rs) (SOME (a,y))),s)) ∧
-  to_thunk s (NestedCase c g gv p e pes) = to_thunk s e ∧
-  to_thunk_list s [] = ([],s) ∧
-  to_thunk_list s (x::xs) =
-    (let (x,s) = to_thunk s x in
-     let (xs,s) = to_thunk_list s xs in
+  to_thunk flag s (NestedCase c g gv p e pes) = to_thunk flag s e ∧
+  to_thunk_list flag s [] = ([],s) ∧
+  to_thunk_list flag s (x::xs) =
+    (let (x,s) = to_thunk flag s x in
+     let (xs,s) = to_thunk_list flag s xs in
        (x::xs,s))
 Termination
   WF_REL_TAC `measure $ λx. case x of
-              | INL x => cexp_size (K 0) (SND x)
-              | INR x => list_size (cexp_size (K 0)) (SND x)`
+              | INL (_,_,e) => cexp_size (K 0) e
+              | INR (_,_,l) => list_size (cexp_size (K 0)) l`
   \\ fs [pure_cexpTheory.cexp_size_eq] \\ rw []
   >~ [‘list_size (cexp_size (K 0)) (MAP SND xs)’] >-
     (pop_assum kall_tac
@@ -106,7 +108,7 @@ End
 
 Definition compile_to_thunk_def:
   compile_to_thunk (c:compiler_opts) e =
-    let (e1, vs) = to_thunk (pure_names e) e in
+    let (e1, vs) = to_thunk c.do_mk_delay (pure_names e) e in
     let (e2, vs2) = split_delated_lam c.do_split_dlam e1 vs in
       e2
 End

commit 15245552c4b785835ba9f60c9074b27c996e12d6

Fix a typo in compiler flags
Introduced by 391fc8e

compiler/backend/passes/pure_comp_confScript.sml
@@ -40,8 +40,8 @@ Definition read_cline_args_def:
     | [] => INL <| do_pure_sort  := ¬ MEM pure_sort_flag cl  ;
                    do_pure_clean := ¬ MEM pure_clean_flag cl ;
                    do_demands    := ¬ MEM demands_flag cl    ;
-                   do_mk_delay   := ¬ MEM dlam_flag cl       ;
-                   do_split_dlam := ¬ MEM mk_delay_flag cl   ;
+                   do_mk_delay   := ¬ MEM mk_delay_flag cl   ;
+                   do_split_dlam := ¬ MEM dlam_flag cl       ;
                    do_app_unit   := ¬ MEM unit_flag cl       |>
 End
 